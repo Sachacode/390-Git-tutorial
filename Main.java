8527d48dded8e1fba46db7afce9b4280e4fd44b3
Sachacode <70292736+Sachacode@users.noreply.github.com>
2022-02-13 19:27:21 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("XD");
+        //Add another fun and engaging print statement
+        System.out.println(";)");
+        
+        //check out my awesome variable
+        int x = 0;
     }
 }
